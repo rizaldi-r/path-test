aa5cf1c077f02d4be5544c384fba3a8f50dc8e26
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-04-18 18:35:27 +0700

    test2

--- module1.js
+++ module1.js
@@ -1,10 +1,11 @@
-import { module2Function } from './src/nodule2.js';
-import { module3Function } from './src/test/module3.js';
+import { module2Function } from "./src/module2.js";
+import { module3Function } from "./src/test/module3.js";
 
 function module1Function() {
-  console.log('module1Function called');
-  module2Function(); // Call function from module2
-  return "module1";
+    console.log("module1Function called");
+    module2Function();
+    module3Function();
+    return "module1";
 }
 
-export { module1Function };
+module1Function;
